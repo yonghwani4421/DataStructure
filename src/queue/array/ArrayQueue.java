505b8059c495d88a9fe9b575235f05b0c92614f3
yonghwan <ghks4421@gmail.com>
2024-06-07 16:47:05 +0900

    arrayqueue 구현

--- src/queue/array/ArrayQueue.java
+++ src/queue/array/ArrayQueue.java
@@ -2,6 +2,9 @@ package queue.array;
 
 import queue.Queue;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * @AUTHOR dyd71
  * @DATE 2024-06-05
@@ -32,18 +35,100 @@ public class ArrayQueue<E> implements Queue<E> {
         this.rear = 0;
     }
 
+    private void resize(int newCapacity){
+        int arrayCapacity = array.length;
+
+        Object[] newArray = new Object[newCapacity];
+
+        for (int i = 1, j = front + 1; i <= size; i++, j++){
+            newArray[i] = array[j % arrayCapacity];
+        }
+
+        this.array = null;
+        this.array = newArray;
+
+        front = 0;
+        rear = size;
+    }
+
     @Override
     public boolean offer(E e) {
-        return false;
+        if ((rear + 1) % array.length == front){
+            resize(array.length * 2);
+        }
+
+        rear = (rear + 1) % array.length;
+
+        array[rear] = e;
+        size++;
+
+        return true;
     }
 
     @Override
     public E poll() {
-        return null;
+        if (size == 0){
+            return null;
+        }
+        front = (front + 1) % array.length;
+        @SuppressWarnings("unchecked")
+        E item = (E) array[front];
+
+        array[front] = null;
+        size--;
+
+        // 사이즈 재설정
+        if (array.length > DEFAULT_CAPACITY && size < (array.length / 4)){
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
+
+        return item;
+    }
+    public E remove(){
+        E item = poll();
+
+        if (item == null){
+            throw new NoSuchElementException();
+        }
+        return item;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (size == 0){
+            return null;
+        }
+        @SuppressWarnings("unchecked")
+        E item = (E) array[(front + 1)% array.length];
+
+        return item;
     }
+    public E element(){
+        E item = peek();
+        if (item == null){
+            throw new NoSuchElementException();
+        }
+        return item;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean contains(Object value){
+        int start = (front + 1) % array.length;
+
+        for (int i = 0, idx = start; i < size; i++, idx = (idx + 1) % array.length){
+            if (array[idx].equals(value)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
